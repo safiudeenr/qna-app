0a1c29667c044f53519c354afb84e81bb030cc03
cn155890 <safiudeen.a.jabbar@centene.com>
2020-04-24 09:31:33 -0500

    modified feed

--- src/app/modules/feed/feed.component.ts
+++ src/app/modules/feed/feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feed',
@@ -7,6 +8,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  searchForm: FormGroup;
+  submitted = true;
+
   feeds = [{
     question: 'ques1',
     answer: 'answer',
@@ -19,17 +23,55 @@ export class FeedComponent implements OnInit {
     community: '123',
     id: 2
   }
-];
+  ];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    console.log(Number(this.route.snapshot.paramMap.get('id')));
+    //console.log(Number(this.route.snapshot.paramMap.get('id')));
+
+    this.searchForm = this.formBuilder.group({
+      searchBox: ['']
+    });
+
   }
+
+  get f() { return this.searchForm.controls; }
 
   getTopPosts() {
     // call service to get all the top posts
   }
 
+  performSearch() {
 
+    if (!this.f.searchBox.value) {
+      return;
+    }
+
+
+    this.feeds = [{
+      question: 'ques1123',
+      answer: 'answer',
+      community: '123',
+      id: 1
+    },
+    {
+      question: 'ques2111',
+      answer: 'answer2',
+      community: '123',
+      id: 2
+    }
+    ];
+  }
+
+
+  addQuestion() {
+    if (!this.f.searchBox.value) {
+      return;
+    }
+
+    
+
+
+  }
 }
